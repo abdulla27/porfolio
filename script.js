e80092712bbdb4e8f28cc69126511b65c9d9b617
abdulla <abdulla27012002@gmail.com>
2023-03-01 16:04:47 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".mutiple-text", {
+  strings: ["Backend Developer", "Frontend Developer", "Fullstack Developer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
